3fbdf6ab77ef64efe4b712bafab5ee8f22023a0e
Muhammad Ali <153639747+muhammadali0313@users.noreply.github.com>
2024-08-16 00:34:34 -0700

    Add files via upload

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-  import { getAuth , createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/10.13/firebase-auth.js';
+  import { getAuth , createUserWithEmailAndPassword, } from 'https://www.gstatic.com/firebasejs/10.13/firebase-auth.js';
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
